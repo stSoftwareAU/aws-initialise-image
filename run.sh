e566636572b337025e357af70c1f0a1656810f1c
AngusLeck <aleck42@gmail.com>
2018-08-07 18:45:13 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,25 @@
 set -e
 #update and install
-sudo yum -y update
-sudo yum -y install jq
-sudo yum -y install git qt
+yum -y update
+yum -y install jq git
+
 curl -O https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
 export PATH=~/.local/bin:$PATH
 pip install awscli --upgrade --user
+
+rm get-pip.py
+
 #get private key from aws secrets manager
 secret_JS=$(aws secretsmanager get-secret-value --secret-id angus-ssh-key --region ap-southeast-2)
 key_pairs_JS=$(jq -r '.SecretString' <<< "${secret_JS}")
 private_key_64=$(jq -r '.private_key' <<< "${key_pairs_JS}")
-echo "${private_key_64}" | base64 -i --decode | zcat > .ssh/id_rsa
-chmod 600 .ssh/id_rsa
+
+mkdir -p /home/ec2_user/.ssh
+echo "${private_key_64}" | base64 -i --decode | zcat > /home/ec2-user/.ssh/id_rsa
+chown -R ec2-user:ec2-user /home/ec2_user/.ssh
+
+chmod 600 /home/ec2-user/.ssh/id_rsa
 #clone st setup from git hub
-git clone git@github.com:stSoftwareAU/st-setup.git
-#st-setup/autodeploy $1
+sudo -u ec2-user git clone git@github.com:stSoftwareAU/st-setup.git
+#sudo -u ec2-user st-setup/autodeploy $1
