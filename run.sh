fda6b03fe31b314247d6c13151d2d4726f9221fb
AngusLeck <aleck42@gmail.com>
2018-08-07 15:16:40 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
-pwd 
-echo "hello world"
+#update and install
+sudo yum -y update
+sudo yum -y install jq
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py --user
+export PATH=~/.local/bin:$PATH
+source ~/. bash profile
+pip install awscli --upgrade --user
+#get private key from aws secrets manager
+secret_JS=$(aws secretsmanager get-secret-value --secret-id angus-ssh-key --region ap-southeast-2)
+key_pairs_JS=$( iq -r '.SecretString' <<< "${secret_JS}")
+private_key_64=$(jq -r '.private_key' <<< "${key_pairs_JS}")
+echo "${private_key_64}" | base64 -i --decode | zcat > .ssh/id_rsa
+#clone st setup from git hub
+git clone git@github.com:stSoftwareAU/st-setup.git
+#st-setup/autodeploy $1
