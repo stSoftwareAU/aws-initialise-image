44d4d863ee1649ce933bc10d50a50ee330e7f551
AngusLeck <aleck42@gmail.com>
2018-08-08 14:27:27 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ rm -f get-pip.py
 secret_JS=$(aws secretsmanager get-secret-value --secret-id github --region ap-southeast-2)
 key_pairs_JS=$(jq -r '.SecretString' <<< "${secret_JS}")
 private_key_64=$(jq -r '.private_key' <<< "${key_pairs_JS}")
-
+exit
 mkdir -p /home/ec2-user/.ssh
 echo "${private_key_64}" | base64 -i --decode | zcat > /home/ec2-user/.ssh/id_rsa
 
@@ -35,5 +35,5 @@ chmod 600 /home/ec2-user/.ssh/id_rsa
 chown -R ec2-user:ec2-user /home/ec2-user/.ssh
 
 #clone st setup from git hub
-sudo -u ec2-user git clone git@github.com:stSoftwareAU/st-setup.git /home/ec2-user/st-setup/
-sudo -u ec2-user /home/ec2-user/st-setup/autodeploy $1
+sudo -u ec2-user git clone git@github.com:stSoftwareAU/st-setup.git
+sudo -u ec2-user st-setup/autodeploy $1
